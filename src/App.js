f2375a014efe9e870b87b27700293ba634b7563f
aaixn <axnguyen246@gmail.com>
2023-01-03 06:56:17 -0800

    login form and localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { React, useEffect, useState } from 'react'
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login.js';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    if (window.localStorage.getItem('token')) {
+      const token = JSON.parse(window.localStorage.getItem('token')) || '{}'
+      if (token) {
+        setUser({
+          ...user,
+          token: token.token,
+          id: token.id,
+          name: token.name,
+          username: token.username,
+          pfp: token.pfp,
+          bio: token.bio
+        })
+      }
+    }
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hiya</h1>
+      <Routes>
+        <Route path='/login' element={<Login user={user} setUser={setUser} />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import './Login.css';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+const Login = ({setUser}) => {
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    const copyForm = {...form}
+    copyForm[e.target.name] = e.target.value
+    setForm(copyForm)
+  }
+
+  const handleLogin = async () => {
+    await axios.post('http://localhost:4000/api/auth/login',
+    {
+      username: form.username,
+      password: form.password
+    })
+    .then((res) => {
+      setUser({...res.data})
+      setForm({...form, username: '', password: ''})
+      if(res.data.token) {
+        window.localStorage.setItem('token', JSON.stringify(res.data))
+        console.log(window.localStorage.getItem('token'))
+      }
+      navigate('/')
+    })
+    .catch(err => console.log(err))
+  }
+
+
+  return (
+    <div>
+      <Box
+        className='login-form'
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <h1>Log In</h1>
+        <TextField className='username-input' id="username" label="Enter Username" variant="outlined" name="username" value={form.username} onChange={handleChange}/>
+        <TextField className='password-input' type="password" id="password" label="Enter Password" variant="outlined" name="password" value={form.password} onChange={handleChange}/>
+        <ButtonGroup className='button-group'>
+          <Button variant='contained' onClick={handleLogin}>Log In</Button>
+          <Button variant='contained'>Sign Up</Button>
+        </ButtonGroup>
+        </Box>
+    </div>
+  )
+}
+
+export default Login
+
